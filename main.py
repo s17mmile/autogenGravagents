32c05ea29c4e66ad56b7b1e8915fa14abb5c184e
s17mmile <s17mmile@uni-bonn.de>
2026-01-19 17:32:10 +0100

    Redid project dir structure, initialized agent-specific class files.

---
+++ main.py
@@ -0,0 +1,23 @@
+from autogen import LLMConfig
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+# BEFORE RUNNING, MAKE SURE TO SET UP YOUR .env FILE WITH AN APPROPRIATE URL AND API KEY
+# In my case, I am using ChatGPT-4o mini through the IZ VPN at the University of Bonn.
+
+# Define LLM configuration to be used for all agent instantiations
+llm_config = LLMConfig(config_list={"api_type": os.getenv("IZ_API_TYPE"), 
+                                    "model": os.getenv("IZ_MODEL"),
+                                    "api_key":os.getenv("IZ_API_KEY"),
+                                    "base_url":os.getenv("IZ_BASE_URL")})
+
+
+
+# Define consistent message/output structure for all agents
+
+
+
+# Set parameters for conversation execution (maximum turns etc)
