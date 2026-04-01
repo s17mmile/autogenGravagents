72281f733ce30c328fc9392d3786ba1b74097bf0
s17mmile <s17mmile@uni-bonn.de>
2026-04-01 23:30:13 +0200

    Small consistency changes for web surfing functionality.

--- flexibleAgents/agentTypes/documentCollectionAgent.py
+++ flexibleAgents/agentTypes/documentCollectionAgent.py
@@ -18,31 +18,31 @@ from autogen.agents.experimental import WebSurferAgent
 # Define document collection agent response format
 class documentCollectionAgentResponse(BaseModel):
 	message: str									# Overview of found documents/visited web pages, important takeaways
-	relevantURLs: List[str]							# List of relevant URLs
+	relevantDocumentURLs: List[str]							# List of relevant URLs
 
 
 
 # The Document Collection Agent is responsible for collecting relevant documents from the web.
 def documentCollectionAgent(llm_config, name = "DocumentCollectionAgent") -> WebSurferAgent:
 	systemMessage = f"""
-		You are a DOCUMENT COLLECTION AGENT specializing in finding documents or data from the web.
+		You are a DOCUMENT COLLECTION AGENT specializing in finding URLs containing useful information from the web.
 		You will find information on the internet and save  URLs to documents or web pages that can be used for Retrieval Augmented Generation.
 		When searching for information, use the most reputable sources possible, such as academic papers, official websites, and well-known news outlets. Avoid using information from unreliable sources or forums if possible.  
 		
 		Your responsibilities:
 		1. Search the web for relevant information based on the queries you receive.
-        2. Save URLs to documents or web pages that can be used for Retrieval Augmented Generation.
-		3. Respond to queries with short summaries of what you found in your search and the URLs of relevant documents or web pages.
+        2. Save URLs to documents or web pages containing high-quality, task-relevant information for Retrieval Augmented Generation.
+		3. Respond to queries with short summaries of what you found in your search and the URLs of query-relevant documents or web pages.
 		4. Do not download the documents yourself, just provide the URLs. The Document Retrieval Agent will handle downloading and ingesting documents based on the URLs you provide.
 		
-		Your output includes a message field and a relevantURLs field:
+		Your output includes a message field and a relevantDocumentURLs field:
 		- The message field should contain an overview of the pages visited and important takeaways to answer the query.
-		- The relevantURLs field should list the URLs of the documents or web pages you found.
+		- The relevantDocumentURLs field should list the URLs of documents or web pages containing the requested information. Do not include search engine result pages, only direct URLs to relevant documents or web pages.
 	"""
 
 	description = """
 		The DOCUMENT COLLECTION AGENT is responsible for finding URLs of relevant documents from the web to be ingested into a retrieval augmented generation system.
-		An RAG system should attempt to download and insget information from these URLs.
+		An RAG system should attempt to download and ingest information from these URLs.
 	"""
 
 	documentCollection_llm_config = llm_config.copy()
